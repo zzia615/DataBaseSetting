222938180b56d5ec47adf3e58cb8dfa278abbc6f
jiaolong <zzia615@163.com>
2021-01-19 10:38:23 +0800

    立项

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace DatabaseSetting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "数据库";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 80);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "用户名";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 115);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(233, 27);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 153);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "密码";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 150);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(233, 27);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 51);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "身份验证";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Windows身份验证",
+            "SqlServer身份验证"});
+            this.comboBox1.Location = new System.Drawing.Point(138, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(233, 25);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("宋体", 13F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "数据库配置修改";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DatabaseSetting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Text = "Windows身份验证";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetFile();
+        }
+
+        string fileName = "";
+        XmlDocument document = new XmlDocument();
+        void GetFile()
+        {
+            DirectoryInfo directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            FileInfo[] files = directory.GetFiles("*.config");
+            if (File.Exists("Web.config"))
+            {
+                fileName = "Web.config";
+            }
+            else if (File.Exists("App.config"))
+            {
+                fileName = "App.config";
+            }
+            else
+            {
+
+                if (files.Length <= 0)
+                {
+
+                    MessageBox.Show("未找到数据库配置文件");
+                    this.Close();
+                    return;
+                }
+                else
+                {
+                    fileName = files[0].FullName;
+                }
+            }
+
+            document.Load(fileName);
+
+            var nodes = document.SelectNodes("/configuration/connectionStrings/add");
+
+            var node = nodes[0];
+
+            string conStr = node.Attributes["connectionString"].InnerText;
+            //去除最后一个分号
+            conStr = conStr.TrimEnd(new char[] { ';' });
+
+            var tmp = conStr.Split(';');
+
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox1.Text = tmp[0].Split('=')[1];
+            textBox2.Text = tmp[1].Split('=')[1];
+            if (tmp.Length == 4)
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                textBox3.Text = tmp[2].Split('=')[1];
+                textBox4.Text = tmp[3].Split('=')[1];
+                comboBox1.Text = "SqlServer身份验证";
+            }
+            if (tmp.Length == 3)
+            {
+                comboBox1.Text = "Windows身份验证";
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Windows身份验证")
+            {
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+            }
+            else
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var nodes = document.SelectNodes("/configuration/connectionStrings/add");
+                var node = nodes[0];
+                string providerName = "System.Data.SqlClient";
+                try
+                {
+                    providerName = node.Attributes["providerName"].InnerText;
+
+                }
+                catch
+                {
+                    var attr = document.CreateAttribute("providerName");
+                    attr.InnerText = providerName;
+                    node.Attributes.Append(attr);
+                }
+
+                var con = System.Data.Common.DbProviderFactories.GetFactory(providerName).CreateConnection();
+                string conStr = "";
+                if (comboBox1.Text == "Windows身份验证")
+                {
+                    conStr = string.Format("Data Source={0};Initial Catalog={1};Integrated Security=true;"
+                        , textBox1.Text, textBox2.Text);
+                }
+                else
+                {
+                    conStr = string.Format("Data Source={0};Initial Catalog={1};UID={2};PWD={3};"
+                        , textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                }
+                con.ConnectionString = conStr;
+                con.Open();
+
+                node.Attributes["connectionString"].InnerText = conStr;
+                document.Save(fileName);
+                MessageBox.Show("保存成功");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
